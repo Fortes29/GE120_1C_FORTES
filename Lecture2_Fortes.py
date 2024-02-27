ff151b7853b32c6be71d3ff823b556de964299cf
Maria Fe Fortes <mfortes@up.edu.ph>
2024-02-27 16:18:28 +0800

    exercise1

---
+++ Lecture2_Fortes.py
@@ -0,0 +1,34 @@
+print("Kumusta Mundo")
+print(type("Kumusta Mundo"))
+print(type(1))
+print(type(str(1)))
+
+print(type("STRING"))
+print(type(54))
+print(type(3.1415265358))
+print(type(1+4j))
+
+print(type(float(54)))
+
+x = 2
+y = 13
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x ** y)
+print(x % y)
+print(abs(y))
+print(float(y))
+print(complex(y))
+print(divmod(y, x))
+
+print("Sum of X and Y: ", x + y)
+
+# lists
+dog = ["golden retriever", "husky", "aspin", "poodle"]
+print(dog)
+print(dog[0])
+
+
