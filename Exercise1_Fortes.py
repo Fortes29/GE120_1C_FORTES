dc911f5d7e6e37846f71e241cb9d5f49e71f0747
Maria Fe Fortes <mfortes@up.edu.ph>
2024-02-27 14:14:04 +0800

    added instruction

---
+++ Exercise1_Fortes.py
@@ -0,0 +1,5 @@
+''' Write a script that accepts decimal degrees (XXX.xxxxxx) as input and outputs the degree, minute, and second (XXX-XX-XX.xx).
+After this, write a script that accepts degree, minutes, second (XXX-XX-XX.xx) as input and outputs the decimal degrees.
+Be sure to include all the necessary comments explaining your code'''
+
+
