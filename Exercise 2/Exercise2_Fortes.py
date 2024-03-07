9e3e0f7384b9d4fde01f32571ac5ab671f52b01d
Maria Fe Fortes <mfortes@up.edu.ph>
2024-03-07 17:20:39 +0800

    exercise 2

---
+++ Exercise 2/Exercise2_Fortes.py
@@ -0,0 +1,19 @@
+'''
+GE 120: Intro to OOP
+Maria Fe Fortes
+
+Machine Exercise 2: 
+Write a script that executes a sentinel-controlled loop that asks for the Distance and Azimuth (in decimal degrees) of a line.
+If yes, the process must repeat until dictated otherwise. 
+Finally, the script must print out a small table showing the line Number, Distance, and Bearing (in DMS, converted from Azimuth in DD) of all of the lines.
+'''
+
+while True:
+    distance = input("Distance: ")
+    azimuth = input("Azimuth: ")
+
+    yn = input("Add new line? ")
+    if yn == "YES":
+        continue
+    else:
+        break
