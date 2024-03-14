1169fd1ddba7efacf857f064e0b5a5f4cc0cb164
Maria Fe Fortes <mfortes@up.edu.ph>
2024-03-14 13:26:21 +0800

    updated

--- Exercise 2/Exercise2_Fortes.py
+++ Exercise 2/Exercise2_Fortes.py
@@ -8,44 +8,45 @@ If yes, the process must repeat until dictated otherwise.
 Finally, the script must print out a small table showing the line Number, Distance, and Bearing (in DMS, converted from Azimuth in DD) of all of the lines.
 '''
 
-counter = 1     # start sa line #1, define muna to 
-lines = []      # empty list, para makuha kung san isstore
+# creating a sentinel-controlled loop
+counter = 1     # to indicate that it starts in line number 1 
+lines = []      # empty list
 
 while True:
     print()
     print("LINE NO.", counter)
     distance = input("Distance: ")
-    while not(bobo_magtype):
-    if bobo_magtype:
-        print
-    azimuth = float(input("Azimuth from the South: ")) % 360
+    azimuth = (input("Azimuth from the South: "))%360
+    if "-" in azimuth:
+        degrees, minutes, seconds = azimuth.split("-")
+        azimuth = (int(degrees) + (int(minutes/60)) + float(round(seconds/3600))) % 360
+    else:
+        azimuth = (azimuth)%360
 
     if azimuth > 0 and azimuth < 90:
         bearing = 'S {: ^10} W'.format(azimuth)
     elif azimuth > 90 and azimuth < 180:
         bearing = 'N {: ^10} W'.format(180 - azimuth)
     elif azimuth > 180 and azimuth < 270: 
-        bearing = azimuth - 180
-
-
-
+        bearing = 'N {: ^10} E'.format(azimuth - 180)
+    elif azimuth > 270 and azimuth < 360:
+        bearing = 'S {: ^10} E'.format(360 - azimuth)
 
     elif azimuth == 0:
-        bearing == "DUE SOUTH"
+        bearing = "DUE SOUTH"
     elif azimuth == 90:
-        bearing == "DUE SOUTH"
-    elif azimuth == 0:
-        bearing == "DUE SOUTH"
-    elif azimuth == 0:
-        bearing == "DUE SOUTH"
-
+        bearing = "DUE WEST"
+    elif azimuth == 180:
+        bearing = "DUE NORTH"
+    elif azimuth == 270:
+        bearing = "DUE EAST"
     else:
-        bearing = "ewan ko"
+        bearing = "not defined"
 
     line = (counter, distance, bearing)     #create tuple of the line
     lines.append(line)
 
-
+    # ask for input
     yn = input("Add new line? ")
     if yn.lower() == "yes" or yn.lower() == "y":
         counter = counter + 1
@@ -57,7 +58,6 @@ print("\n\n")
 print('{: ^10} {: ^10} {: ^10}'.format("LINE NO.", "DISTANCE", "BEARING"))
 
 for line in lines:
-    print('{} {} {}'.format(line[0], line[1], line[2]))
+    print('{: ^10} {: ^10} {: ^10}'.format(line[0], line[1], line[2]))
 
-print(lines)
 print("----END----")
