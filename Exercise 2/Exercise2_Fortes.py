79c7b19d5ea202c7d43fbf8d3516881c3dc1fc4d
Maria Fe Fortes <mfortes@up.edu.ph>
2024-03-15 02:23:47 +0800

    added comments

--- Exercise 2/Exercise2_Fortes.py
+++ Exercise 2/Exercise2_Fortes.py
@@ -5,32 +5,34 @@ Maria Fe Fortes
 Machine Exercise 2: 
 Write a script that executes a sentinel-controlled loop that asks for the Distance and Azimuth (in decimal degrees) of a line.
 If yes, the process must repeat until dictated otherwise. 
-Finally, the script must print out a small table showing the line Number, Distance, and Bearing (in DMS, converted from Azimuth in DD) of all of the lines.
+Finally, the script must print out a small table showing the 
+Line Number, Distance, and Bearing (in DMS, converted from Azimuth in DD) of all of the lines.
 '''
 
 # creating a sentinel-controlled loop
-counter = 1     # to indicate that it starts in line number 1 
-lines = []      # empty list
 
+counter = 1     # to indicate that it starts in line number 1 
+lines = []      # empty list to store line data  
+
+# loop to input data
 while True:
+    # to display inputted line number
     print()
     print("LINE NO.", counter)
-    distance = input("Distance: ")
-    azimuth = (input("Azimuth from the South: "))%360
-    if "-" in azimuth:
-        degrees, minutes, seconds = azimuth.split("-")
-        azimuth = (int(degrees) + (int(minutes/60)) + float(round(seconds/3600))) % 360
-    else:
-        azimuth = (azimuth)%360
+    
+    # user input
+    distance = input("Distance: ")  
+    azimuth = float(input("Azimuth from the South: ")) % 360  # using modulo to return the remainder
 
-    if azimuth > 0 and azimuth < 90:
-        bearing = 'S {: ^10} W'.format(azimuth)
-    elif azimuth > 90 and azimuth < 180:
-        bearing = 'N {: ^10} W'.format(180 - azimuth)
-    elif azimuth > 180 and azimuth < 270: 
-        bearing = 'N {: ^10} E'.format(azimuth - 180)
-    elif azimuth > 270 and azimuth < 360:
-        bearing = 'S {: ^10} E'.format(360 - azimuth)
+    # using elif for multiple conditions to specify if the preceding 'if' condition is false
+    if azimuth < 90:
+        bearing = 'S {: ^10} W'.format(azimuth)         # if azimuth is 0-90, bearing is equal to the azimuth and SW is the direction
+    elif azimuth < 180:                                  
+        bearing = 'N {: ^10} W'.format(180 - azimuth)   # if azimuth is 90-180, subtracting azimuth from 180 gives the bearing, NW is the direction
+    elif azimuth < 270: 
+        bearing = 'N {: ^10} E'.format(azimuth - 180)   # if azimuth is 180-270, subtracting 180 from azimuth gives the bearing, NE is the direction
+    elif azimuth < 360:
+        bearing = 'S {: ^10} E'.format(360 - azimuth)   # if azimuth is 270-360, subtracting azimuth from 360 gives the bearing, SE is the direction
 
     elif azimuth == 0:
         bearing = "DUE SOUTH"
@@ -43,21 +45,23 @@ while True:
     else:
         bearing = "not defined"
 
-    line = (counter, distance, bearing)     #create tuple of the line
-    lines.append(line)
+    line = (counter, distance, bearing)     # create tuple of the line
+    lines.append(line)  # append line data to the list
 
-    # ask for input
-    yn = input("Add new line? ")
-    if yn.lower() == "yes" or yn.lower() == "y":
-        counter = counter + 1
+    # to enter data for another line
+    x = input("Add new line? ")
+    if x.lower() == "yes" or x.lower() == "y":
+        counter = counter + 1   # increasing line number for next iteration
         continue
-    else:
+    else:   # break the loop if the user's input is not yes or y
         break
 
-print("\n\n")
-print('{: ^10} {: ^10} {: ^10}'.format("LINE NO.", "DISTANCE", "BEARING"))
+print("\n\n")   # two consecutive newlines causing a blank line printed between each inputted line number
+print('{: ^10} {: ^20} {: ^15}'.format("LINE NO.", "DISTANCE (m)", "BEARING")) # to format the line data centered within a width of (given number) characters  
 
-for line in lines:
-    print('{: ^10} {: ^10} {: ^10}'.format(line[0], line[1], line[2]))
+# iterate over each line in the lines list
+# print data for each line in a table format
+for line in lines: 
+    print('{: ^10} {: ^20} {: ^10}'.format(line[0], line[1], line[2])) # to format the line data centered within a width of (given number) characters
 
-print("----END----")
+print("----------END----------")
